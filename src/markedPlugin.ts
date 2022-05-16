1a617ad76ba7efdedd0fbd0bc1ec156a2b99164a
serverhiccups <hiccup@hiccup01.com>
2022-05-16 19:16:31 +0200

    automatically insert width and height properties on images

--- src/markedPlugin.ts
+++ src/markedPlugin.ts
@@ -1,17 +1,41 @@
+import imageSize from "probe-image-size";
+import { readFileSync } from "fs";
+import { resolve } from "path";
+
 const imageRenderer = {
 	image(href: string, title: string | null, text: string) {
+		let sizeHints = {
+			width: null,
+			height: null,
+		};
+		if (href.startsWith("images/")) {
+			// is probably a local file?
+			console.log(`image with path ${href}`);
+			console.log(resolve("./public/" + href));
+			try {
+				const f = readFileSync(resolve("./public/" + decodeURIComponent(href)));
+				const info = imageSize.sync(f);
+				sizeHints.width = info.width;
+				sizeHints.height = info.height;
+			} catch (err) {
+				console.error("oh noes!"); // TODO: replace with a div on the generated page
+				console.error(err);
+			}
+		}
 		return `
 			<figure class="img-container">
-				<img loading="lazy" src="${href}" alt="title">
+				<img loading="lazy" src="${href}" alt="title" ${
+			sizeHints.width ? 'width="' + sizeHints.width + '"' : ""
+		} ${sizeHints.height ? 'height="' + sizeHints.height + '"' : ""}>
 				${title != null ? `<figcaption>${title}</figcaption>` : ""}
 			</figure>
 		 `;
-	}
-}
+	},
+};
 
 const galleryExtension = {
 	name: "gallery",
-	level: 'block',
+	level: "block",
 	start(src: string) {
 		return src.match(/^\$\$/)?.index;
 	},
@@ -23,30 +47,33 @@ const galleryExtension = {
 				type: "gallery",
 				raw: match[0],
 				text: match[1].trim(),
-				tokens: []
-			}
+				tokens: [],
+			};
 			token.text.split("\n").forEach((i) => {
 				this.lexer.inline(i, token.tokens);
-			})
+			});
 			return token;
 		}
 	},
 	renderer(token: any) {
 		return `
 		<div class="image-gallery">
-		${token.tokens.filter((t) => {
-			return t.type == "image"
-		}).map((i) => {
-			return `
+		${token.tokens
+			.filter((t) => {
+				return t.type == "image";
+			})
+			.map((i) => {
+				return `
 			<div class="image-gallery-slide">
 				<img loading="lazy" src="${i.href}" ${i.title ? 'alt="${i.title}"' : ""}">
 				${i.title ? `<span>${i.title}</span>` : ""}
 			</div>
-			`
-		}).join("")}
+			`;
+			})
+			.join("")}
 		</div>
-		`
-	}
-}
+		`;
+	},
+};
 
-export { imageRenderer, galleryExtension }
+export { imageRenderer, galleryExtension };
